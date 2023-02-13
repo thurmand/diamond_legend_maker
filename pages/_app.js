ba6169314c232f813701d2f84bb3228aa234a364
Andrew <twintrool@gmail.com>
2023-02-12 23:05:15 -0600

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "tailwindcss/tailwind.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
